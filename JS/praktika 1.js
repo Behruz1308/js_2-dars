208a013006e37415346d27a2f38f49874cb2d875
Behruz <xujamurodovbehruz@gmail.com>
2023-05-20 09:00:54 +0500

    js 2-dars
    js 2-dars kodlari yuklandi!

---
+++ JS/praktika 1.js
@@ -0,0 +1,63 @@
+let a = 264+303;
+let b = 108-72;
+let c = 19*14;
+let d = 255/3;
+
+if (a==567 && b==26 && c==266 && d==20){
+    console.log("Ular teng!");
+}
+else{
+    console.log("Ular teng emas!");
+}
+
+if (a==567 && b==35 && c==936 && d==85){
+    console.log("Ular o'zaro teng!");
+}
+else{
+    console.log("Ular o'zaro teng emas!");
+}
+
+let e = "iPhone";
+let f = "Samsung";
+let g = "Inoi";
+let h = "RedMi";
+
+if (e=="inoi" && f=="redmi" && g=="Samsung" && h=="iPhone"){
+    console.log("Ha ular teng teng!");
+}
+else{
+    console.log("Yo'q Iphone ularga teng emas!");
+}
+
+let i = 41;
+let j = 2;
+let k = 43;
+let l = 991;
+
+if (i==41 && j==2 && k==43 && l==990){
+    console.log("Sonlar teng!")
+}
+else{
+    console.log("Ular teng emas, chunki L 990ga teng emas")
+}
+
+let m = "Bilolxo'ja";
+let n = "Behruz";
+let o = "Erjan";
+let p = "Abduqodir";
+
+if (m=="Bilolxo'ja" && n=="Behruz" && o=="Erjan" && p=="Abduqodir aka"){
+    console.log("Ular ozaro teng!")
+}
+else{
+    console.log("Yo'q Bilolxoja ulardan IT ni yahshiroq biladi!")
+}
+
+
+
+
+
+
+
+
+
---
+++ JS/praktika 2.js
@@ -0,0 +1,56 @@
+let a = 13;
+let b = 8;
+let c = 20;
+let d = 10;
+
+if (a==30 || b==48 || c==11 || d==425){
+    console.log("Ular biribiriga teng emas!")
+}
+else{
+    console.log("Ular o'zaro teng!")
+}
+
+let e = 112;
+let f = 417;
+let g = 110;
+let h = 4124;
+
+if (e==12 || f==48 || g==10 || h==424){
+    console.log("Ular o'zaro teng!")
+}
+else{
+    console.log("Ular o'zaro teng emas!")
+}
+
+let i = "*";
+let j = "/";
+let k = "+";
+let l = "-";
+
+if (i=="+" || j=="-" || k=="*" || l=="/"){
+    console.log("Ular navbatma navbat bajariladi!")
+}
+else{
+    console.log("Birinchi ko'paytiruv keyin bo'luv bajariladi undan keyin esa qo'shish va ayirish bajariladi!")
+}
+
+let m = "hp";
+let n = "Apple";
+let o = "Asus";
+let p = "Samsung";
+
+if (m=="Apple" || n=="hp" || o=="Samsung" || p=="Asus"){
+    console.log("Apple hp ga teng! Asus Samsung ga teng!")
+}
+else {
+    console.log("Apple hp ga teng emas! Asus Samsung ga teng emas!")
+}
+
+
+
+
+
+
+
+
+
